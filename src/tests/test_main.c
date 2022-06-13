2dc13754e3e3102d436554ee8374cb8fbf110274
Werlag Deckard <werlagde@student.21-school.ru>
2022-06-13 14:49:12 +0300

    Corrected loop in main test files

--- src/tests/test_main.c
+++ src/tests/test_main.c
@@ -10,8 +10,8 @@ int main(void) {
     srunner_set_fork_status(sr, CK_NOFORK);
     int nf;
 
-    // for (size_t i = 0; i < ARRAY_SIZE(test_suites) - 1; i++)
-    //     srunner_add_suite(sr, test_suites[i]);
+    for (size_t i = 1; i < ARRAY_SIZE(test_suites); i++)
+        srunner_add_suite(sr, test_suites[i]);
 
     srunner_run_all(sr, CK_ENV);
     nf = srunner_ntests_failed(sr);
