fc67fbe22b638881cec69b6a51fd322b82e7538e
Werlag Deckard <werlagde@student.21-school.ru>
2022-06-13 13:33:21 +0300

    Completed s21_exp with original fabsl

--- src/tests/test_main.c
+++ src/tests/test_main.c
@@ -3,15 +3,16 @@
 
 int main(void) {
     Suite* test_suites[] = {
-        suite_s21_abs()
+        suite_s21_abs(),
+        suite_s21_exp(),
     };
 
     SRunner *sr = srunner_create(test_suites[0]);
     srunner_set_fork_status(sr, CK_NOFORK);
     int nf;
 
-    // for (size_t i = 0; i < ARRAY_SIZE(test_suites) - 1; i++)
-    //     srunner_add_suite(sr, test_suites[i]);
+    for (size_t i = 1; i < ARRAY_SIZE(test_suites); i++)
+        srunner_add_suite(sr, test_suites[i]);
 
     srunner_run_all(sr, CK_ENV);
     nf = srunner_ntests_failed(sr);
