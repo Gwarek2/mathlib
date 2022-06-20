b78febe5c2c73f0892e5d257b40a7a613fda0bf8
Gwarek2 <68369930+Gwarek2@users.noreply.github.com>
2022-06-20 18:26:19 +0300

    Update s21_abs_test.c

--- src/tests/s21_abs_test.c
+++ src/tests/s21_abs_test.c
@@ -10,13 +10,13 @@ START_TEST(test_pos) { ck_assert_int_eq(abs(1234), s21_abs(1234)); }
 END_TEST
 
 Suite *suite_s21_abs(void) {
-  Suite *s = suite_create("s21_abs");
-  TCase *tc = tcase_create("core");
+    Suite *s = suite_create("s21_abs");
+    TCase *tc = tcase_create("core");
 
-  tcase_add_test(tc, test_zero);
-  tcase_add_test(tc, test_neg);
-  tcase_add_test(tc, test_pos);
+    tcase_add_test(tc, test_zero);
+    tcase_add_test(tc, test_neg);
+    tcase_add_test(tc, test_pos);
 
-  suite_add_tcase(s, tc);
-  return s;
+    suite_add_tcase(s, tc);
+    return s;
 }
