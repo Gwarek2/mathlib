9478fc8d7e349c7de6511ba3427889822d72648c
Knew1t <saen.it@yandex.ru>
2022-06-23 18:27:00 +0300

    new branch

---
+++ src/lib/s21_cos.c
@@ -0,0 +1,27 @@
+#include "../s21_math.h"
+long double _factorial(double num);
+long double s21_cos(double num) {
+    long double result = 0;
+    if (!(is_inf(num) || is_nan(num))) {
+        num = s21_fmod(num, S21_PI);
+        for (int i = 0; i < 50; i++) {
+
+            result += s21_pow(-1, i) * s21_pow(num, 2 * i) / _factorial(2 * i);
+        }
+    } else {
+        return S21_NAN;
+    }
+    return result;
+}
+
+long double _factorial(double num) {
+    long double result;
+    if (num < 0)
+        result = 0;
+    if (num == 0)
+        result = 1;
+    else
+        result = num * _factorial(num - 1);
+
+    return result;
+}
---
+++ src/tests/s21_cos_test.c
@@ -0,0 +1,95 @@
+#include "test_main.h"
+static const double cos_EPS = 1e-16l;
+
+START_TEST(test_cos_positive) {
+    double x = 0.4;
+    ck_assert_ldouble_eq_tol(cos(x), s21_cos(x), TEST_EPS);
+}
+END_TEST
+
+START_TEST(test_cos_negative) {
+    double x = -0.4;
+    ck_assert_ldouble_eq_tol(cos(x), s21_cos(x), TEST_EPS);
+}
+END_TEST
+
+START_TEST(test_cos_zero) {
+    double x = 0;
+    ck_assert_ldouble_eq_tol(cos(x), s21_cos(x), TEST_EPS);
+}
+END_TEST
+
+START_TEST(test_cos_one) {
+    double x = 1;
+    ck_assert_ldouble_eq_tol(cos(x), s21_cos(x), TEST_EPS);
+}
+END_TEST
+
+START_TEST(test_cos_one_negative) {
+    double x = -1;
+    ck_assert_ldouble_eq_tol(cos(x), s21_cos(x), TEST_EPS);
+}
+END_TEST
+
+START_TEST(test_cos_huge_positive) {
+    double x = 598987986;
+    ck_assert_ldouble_nan(s21_cos(x));
+}
+END_TEST
+
+START_TEST(test_cos_huge_negative) {
+    double x = -5938409234;
+    ck_assert_ldouble_nan(s21_cos(x));
+}
+END_TEST
+
+START_TEST(test_cos_big_mantissa) {
+    double x = 0.123456789012345678901234567890;
+    ck_assert_ldouble_eq_tol(cos(x), s21_cos(x), TEST_EPS);
+}
+END_TEST
+START_TEST(test_cos_max_dbl) {
+    double x = DBL_MAX;
+    ck_assert_ldouble_eq_tol(cos(x), s21_cos(x), TEST_EPS);
+}
+END_TEST
+
+START_TEST(test_cos_eps_positive) {
+    double x = 1 + cos_EPS;
+    ck_assert_ldouble_eq_tol(cos(x), s21_cos(x), TEST_EPS);
+}
+END_TEST
+
+START_TEST(test_cos_eps_negative) {
+    double x = -1 - cos_EPS;
+    ck_assert_ldouble_eq_tol(cos(x), s21_cos(x), TEST_EPS);
+}
+END_TEST
+START_TEST(test_cos_inf) { ck_assert_ldouble_nan(s21_cos(INFINITY)); }
+END_TEST
+START_TEST(test_cos_nan) { ck_assert_ldouble_nan(s21_cos(NAN)); }
+END_TEST
+START_TEST(test_cos_minus_inf) { ck_assert_ldouble_nan(s21_cos(-INFINITY)); }
+END_TEST
+Suite *suite_s21_cos(void) {
+    Suite *s = suite_create("s21_cos");
+    TCase *tc = tcase_create("core");
+
+    tcase_add_test(tc, test_cos_positive);
+    tcase_add_test(tc, test_cos_negative);
+    tcase_add_test(tc, test_cos_zero);
+    tcase_add_test(tc, test_cos_one);
+    tcase_add_test(tc, test_cos_negative);
+    tcase_add_test(tc, test_cos_one_negative);
+    tcase_add_test(tc, test_cos_huge_positive);
+    tcase_add_test(tc, test_cos_huge_negative);
+    tcase_add_test(tc, test_cos_big_mantissa);
+    tcase_add_test(tc, test_cos_eps_positive);
+    tcase_add_test(tc, test_cos_eps_negative);
+    tcase_add_test(tc, test_cos_inf);
+    tcase_add_test(tc, test_cos_nan);
+    tcase_add_test(tc, test_cos_minus_inf);
+    tcase_add_test(tc, test_cos_max_dbl);
+    suite_add_tcase(s, tc);
+    return s;
+}
