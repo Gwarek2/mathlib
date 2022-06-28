78806ff20e3bb0fbdc1e5e7b44a5aa1b26a71669
Longshot Lynsey <longshot@student.21-school.ru>
2022-06-28 16:08:34 +0300

    tan completed

--- src/tests/test_main.c
+++ src/tests/test_main.c
@@ -1,14 +1,9 @@
 #include "test_main.h"
 
 int main(void) {
-    Suite *test_suites[] = {suite_s21_abs(),  suite_s21_exp(),
-                            suite_s21_log(),  suite_s21_pow(),
-                            suite_s21_sqrt(), suite_s21_asin(),
-                            suite_s21_ceil(), suite_s21_floor(),
-                            suite_s21_fabs(), suite_s21_trunc(),
-                            suite_s21_sin(), suite_s21_fmod(),
-                            suite_s21_acos(), suite_s21_cos()};
-
+    Suite *test_suites[] = {suite_s21_abs(),  suite_s21_exp(),  suite_s21_log(),   suite_s21_pow(),  suite_s21_sqrt(),
+                            suite_s21_asin(), suite_s21_ceil(), suite_s21_floor(), suite_s21_fabs(), suite_s21_trunc(),
+                            suite_s21_sin(),  suite_s21_fmod(), suite_s21_acos(),  suite_s21_cos(),  suite_s21_tan()};
 
     SRunner *sr = srunner_create(test_suites[0]);
     srunner_set_fork_status(sr, CK_NOFORK);
