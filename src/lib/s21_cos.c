50ca37678de1f7985f9b62d928b09a619d07ec0f
Gwarek2 <68369930+Gwarek2@users.noreply.github.com>
2022-06-30 12:33:01 +0300

    Refactored Makefile (#28)

--- src/lib/s21_cos.c
+++ src/lib/s21_cos.c
@@ -5,7 +5,7 @@ static const double COS_EPS = 1e-6l;
 long double s21_cos(double x) {
     double result = 0;
     if (!is_inf(x) && !is_nan(x)) {
-        if (s21_fabs(x) >= COS_EPS) x = s21_fmod(x, 2 * S21_PI);
+        x = s21_fmod(x, 2 * S21_PI);
         double i = 1;
         double step = 1;
         result = 1;
